38d7ea652f0c0efc4ccce336edd7aa1756d0667e
Yetrina Battad <hello@yetti.io>
2023-03-14 07:54:41 +1100

    fix: move flipper-ui into finding-aids scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   mount Flipper::UI.app(Flipper) => "/feats", :constraints => StaffOnlyConstraint.new
 
+  # for non-local Arclight deployment
+  scope(path: "/finding-aids") do
+    mount Flipper::UI.app(Flipper) => "/feats", :constraints => StaffOnlyConstraint.new
+  end
+
   devise_for :users, controllers: {
     sessions: "users/sessions",
     omniauth_callbacks: "users/omniauth_callbacks"
