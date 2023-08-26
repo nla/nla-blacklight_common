6b0b7ec019f84338770c6f14f77493261f32f63b
Yetrina Battad <hello@yetti.io>
2023-08-26 23:36:17 +1000

    chore: change session cookie security

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Rails.application.config.session_store :active_record_store, key: "_catalogue_session", secure: Rails.env.production?, httponly: true, samesite: {lax: true}
+Rails.application.config.session_store :active_record_store, key: "_catalogue_session", secure: Rails.env.production? || Rails.env.staging?, httponly: true, samesite: {lax: true}
 
 # This needs to be set after initialisation, otherwise the model classes won't be loaded
 # and an error will be thrown regarding CatalogueSession not being found.
