4585bef8dd85f80109ca031013bd355c64fee739
Yetrina Battad <hello@yetti.io>
2023-10-10 14:35:51 +1100

    feat: exclude failed urls when retrying Solr urls

--- lib/blacklight/solr_cloud/repository.rb
+++ lib/blacklight/solr_cloud/repository.rb
@@ -21,9 +21,9 @@ module Blacklight
         make_connection(0)
       end
 
-      def make_connection(tries)
+      def make_connection(tries, failed_urls = [])
         synchronize do
-          connection = ZK.open(connection_config[:zk_host], {chroot: :do_nothing}) do |zk|
+          url = ZK.open(connection_config[:zk_host], {chroot: :do_nothing}) do |zk|
             collection_state_json, _stat = zk.get("/collections/#{connection_config[:collection]}/state.json", watch: false)
             collection_state = ::JSON.parse(collection_state_json)[connection_config[:collection]]
 
@@ -36,21 +36,21 @@ module Blacklight
             all_urls = []
             all_nodes.flatten&.each do |node|
               next unless live_nodes.include?(node["node_name"]) && node["state"] == ACTIVE
-              all_urls << "#{node["base_url"]}/#{connection_config[:collection]}"
+              all_urls << "#{node["base_url"]}/#{connection_config[:collection]}" if failed_urls.exclude? node["base_url"]
             end
 
             url = all_urls.sample
             raise Blacklight::SolrCloud::NotEnoughNodes unless url
-
-            GC.start
-
-            ::RSolr.connect(connection_config.merge(adapter: connection_config[:http_adapter], url: url, timeout: 30, open_timeout: 30))
           end
 
-          connection.presence
-        rescue
-          tries += 1
-          (tries > MAX_RETRIES) ? raise(Blacklight::SolrCloud::NotEnoughNodes) : make_connection(tries)
+          begin
+            client = ::RSolr.connect(connection_config.merge(adapter: connection_config[:http_adapter], url: url, timeout: 30, open_timeout: 30))
+            client.presence
+          rescue
+            tries += 1
+            failed_urls << url
+            (tries > MAX_RETRIES) ? raise(Blacklight::SolrCloud::NotEnoughNodes) : make_connection(tries, failed_urls)
+          end
         end
       end
     end
