b6eca1dd6a8bd3d252654303aa5e19336d327f34
Yetrina Battad <hello@yetti.io>
2023-11-07 13:44:58 +1100

    chore: remove keycloak patron login flag

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   # Attempt to find the mapped route for devise based on request path
 
-  if ENV["KC_PATRON_REALM"]
-    include AuthSessionConcern
-  end
+  include AuthSessionConcern
 end
