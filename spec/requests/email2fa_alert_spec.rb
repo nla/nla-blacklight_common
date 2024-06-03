afd14b6a349e5bab9c282baae908b09f6fc2b3f3
Yetrina Battad <hello@yetti.io>
2024-06-03 14:10:20 +1000

    fix: only show Email 2FA alert to patrons

--- spec/requests/email2fa_alert_spec.rb
+++ spec/requests/email2fa_alert_spec.rb
@@ -37,6 +37,25 @@ RSpec.describe "Email2faAlerts" do
       expect(response).to render_template(:show)
       expect(response.body).to include("Would you like to further secure your NLA user account with Two-factor Authentication (2FA)?")
     end
+
+    context "when user is a staff member" do
+      before do
+        user = create(:user, :staff)
+        sign_in user
+      end
+
+      it "does not display the alert" do
+        # rubocop:disable RSpec/VerifiedDoubles
+        allow(Email2faService).to receive(:new).and_return(double(email_2fa_status: false))
+        # rubocop:enable RSpec/VerifiedDoubles
+
+        get email_2fa_alert_path
+
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:show)
+        expect(response.body).not_to include("Would you like to further secure your NLA user account with Two-factor Authentication (2FA)?")
+      end
+    end
   end
 
   describe "GET /dismiss" do
--- spec/requests/email2fa_spec.rb
+++ spec/requests/email2fa_spec.rb
@@ -2,35 +2,67 @@ require "rails_helper"
 
 RSpec.describe "Email2fa" do
   describe "GET /enable" do
-    it "redirects to the email 2fa registration url" do
+    before do
       allow(ENV).to receive(:[]).with("KEYCLOAK_URL").and_return("https://keycloak.example.com")
       allow(ENV).to receive(:[]).with("KC_PATRON_REALM").and_return("patron")
       allow(ENV).to receive(:[]).with("KC_PATRON_CLIENT").and_return("patron-client")
       allow(ENV).to receive(:[]).with("EMAIL_2FA_REGISTRATION_REDIRECT_URL").and_return("https://www.example.com")
       allow(ENV).to receive(:[]).with("EMAIL_2FA_ACTION").and_return("register")
       allow(ENV).to receive(:[]).with("EMAIL_2FA_REGISTRATION_URL").and_return("%s/auth/realms/%s/protocol/openid-connect/auth?response_type=code&client_id=%s&scope=openid&redirect_uri=%s&kc_action=%s")
+    end
 
-      get email_2fa_enable_path
+    context "when logged in" do
+      before do
+        patron = create(:user)
+        sign_in patron
+      end
 
-      expect(response).to redirect_to("https://keycloak.example.com/auth/realms/patron/protocol/openid-connect/auth?response_type=code&client_id=patron-client&scope=openid&redirect_uri=https://www.example.com&kc_action=register")
+      it "redirects to the email 2fa registration url" do
+        get email_2fa_enable_path
+
+        expect(response).to redirect_to("https://keycloak.example.com/auth/realms/patron/protocol/openid-connect/auth?response_type=code&client_id=patron-client&scope=openid&redirect_uri=https://www.example.com&kc_action=register")
+      end
+    end
+
+    context "when logged out" do
+      it "redirects to the login page" do
+        get email_2fa_enable_path
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 
   describe "GET /disable" do
-    before do
-      patron = create(:user)
-      sign_in patron
+    context "when logged in" do
+      before do
+        patron = create(:user)
+        sign_in patron
+      end
+
+      it "redirects back to the redirect url" do
+        # rubocop:disable RSpec/VerifiedDoubles
+        allow(Email2faService).to receive(:new).and_return(double(disable_email_2fa: nil, email_2fa_status: nil))
+        # rubocop:enable RSpec/VerifiedDoubles
+        allow(ENV).to receive(:[]).with("EMAIL_2FA_REGISTRATION_REDIRECT_URL").and_return("https://www.example.com")
+
+        get email_2fa_disable_path
+
+        expect(response).to redirect_to("https://www.example.com")
+      end
     end
 
-    it "redirects back to the redirect url" do
-      # rubocop:disable RSpec/VerifiedDoubles
-      allow(Email2faService).to receive(:new).and_return(double(disable_email_2fa: nil, email_2fa_status: nil))
-      # rubocop:enable RSpec/VerifiedDoubles
-      allow(ENV).to receive(:[]).with("EMAIL_2FA_REGISTRATION_REDIRECT_URL").and_return("https://www.example.com")
+    context "when logged out" do
+      it "redirects back to the redirect url" do
+        # rubocop:disable RSpec/VerifiedDoubles
+        allow(Email2faService).to receive(:new).and_return(double(disable_email_2fa: nil, email_2fa_status: nil))
+        # rubocop:enable RSpec/VerifiedDoubles
+        allow(ENV).to receive(:[]).with("EMAIL_2FA_REGISTRATION_REDIRECT_URL").and_return("https://www.example.com")
 
-      get email_2fa_disable_path
+        get email_2fa_disable_path
 
-      expect(response).to redirect_to("https://www.example.com")
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
   end
 end
