5c00fca4be29a7d4b80dfe203b09685fffaae444
calumedwardsblasikiewicz <cedwards@nla.gov.au>
2025-04-08 10:31:19 +1000

    test fix migration errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,11 +30,11 @@ Dir[Rails.root.join("..", "..", "spec", "support", "**", "*.rb")].sort.each { |f
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
-end
+# begin
+#   ActiveRecord::Migration.maintain_test_schema!
+# rescue ActiveRecord::PendingMigrationError => e
+#   abort e.to_s.strip
+# end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = "#{::Rails.root}/spec/fixtures"
